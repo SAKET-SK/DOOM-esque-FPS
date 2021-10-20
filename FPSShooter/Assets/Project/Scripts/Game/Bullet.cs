693347af198b83a74acb391257b320e8a6c083ed
Saket Khopkar <saketkhopkar478@gmail.com>
2021-10-20 11:54:59 +0530

    Doom lookalike FPS game files

---
+++ FPSShooter/Assets/Project/Scripts/Game/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 8f;
+	public float lifeDuration = 2f;
+	public int damage = 5;
+
+	private float lifeTimer;
+
+	private bool shotByPlayer;
+	public bool ShotByPlayer { get { return shotByPlayer; } set { shotByPlayer = value; } }
+
+	// Use this for initialization
+	void OnEnable () {
+		lifeTimer = lifeDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Make the bullet move.
+		transform.position += transform.forward * speed * Time.deltaTime;
+
+		// Check if the bullet should be destroyed.
+		lifeTimer -= Time.deltaTime;
+		if (lifeTimer <= 0f) {
+			gameObject.SetActive (false);
+		}
+	}
+}
---
+++ FPSShooter/Assets/Project/Scripts/Menu/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    void Awake()
+    {
+        // To show cursor after First Person Controller
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void OnPlay () {
+		SceneManager.LoadScene ("Level1");
+	}
+}
